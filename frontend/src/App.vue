5b2aa78a7b93b61fdb04817529c91934997c93ad
web3摸鱼者 <121859606+nft-maker-one@users.noreply.github.com>
2024-05-30 09:41:09 +0800

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <router-view/>
+  </el-config-provider>
+</template>
+
+<script setup>
+import {ElConfigProvider} from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+
+const debounce = (fn, delay) => {
+  let timer
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
+}
+
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback) {
+    callback = debounce(callback, 200);
+    super(callback);
+  }
+}
+</script>
+
+
+<style lang="stylus">
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  margin: 0 !important;
+  padding: 0 !important;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+.el-overlay-dialog {
+  display flex
+  justify-content center
+  align-items center
+  overflow hidden
+
+  .el-dialog {
+    margin 0;
+
+    .el-dialog__body {
+      max-height 90vh
+    }
+  }
+}
+
+/* 省略显示 */
+.ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.van-toast--fail {
+  background #fef0f0
+  color #f56c6c
+}
+
+.van-toast--success {
+  background #D6FBCC
+  color #07C160
+}
+
+</style>
